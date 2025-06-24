393cbca8b8929800b25b56933212f4d96fed9394
iainv <iainvisa@gmail.com>
2025-06-24 15:12:53 +0200

    ARDB.py

--- scripts/ARDB.py
+++ scripts/ARDB.py
@@ -3,3 +3,23 @@ import numpy as np
 import pandas as pd
 from utilities import *
 from Globals import root, local, vars
+
+
+# ARDB: https://androgendb.mcgill.ca/
+# Citation to ARDB:
+# Gottlieb B, Beitel LK, Nadarajah A, Palioura M, Trifiro M. 2012. The Androgen Receptor Gene Mutations Database(ARDB):2012 Update Human Mutation 33:887-894.
+# https://onlinelibrary.wiley.com/doi/full/10.1002/humu.22046
+
+
+
+
+
+
+
+
+
+
+
+
+
+
